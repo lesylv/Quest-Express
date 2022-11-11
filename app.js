15316f626482022853fc5901cc1f13215609cc5c
Tormo <sylvaain11@hotmail.fr>
2022-11-11 17:18:27 +0100

    correction quest

--- app.js
+++ app.js
@@ -15,6 +15,12 @@ const movieHandlers = require("./movieHandlers");
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
+const usersHandlers = require("./usersHandlers");
+
+app.get("/api/users", usersHandlers.getUsers);
+app.get("/api/users/:id", usersHandlers.getUserById);
+
+
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
--- database.js
+++ database.js
@@ -1,10 +1,5 @@
 require("dotenv").config();
-
-const port = process.env.APP_PORT ?? 5000;
-
-const { app } = require("express");
 const mysql = require("mysql2/promise");
-
 
 const database = mysql.createPool({
   host: process.env.DB_HOST,
@@ -15,36 +10,14 @@ const database = mysql.createPool({
 });
 
 
-const getUser = (req, res) => {
 
-  const id = parseInt(req.params.id);
-  database
-    .query("select * from users where id = ?", [id])
-    .then(([users]) => {
-      if (users[0] != null) {
-        res.json(users[0]);
-      } else {
-        res.status(404).send('Not found');
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error retrieving data from database");
-    });
-};
-
-app.get("/api/users", getUser);
-app.get("/api/users/:id", getUser);
-
-
-app.listen(port, (err) => {
-  if (err) {
-    console.error("Something bad happened");
-  } else {
-    console.log(`Server is listening on ${port}`);
-  }
-});
-
-
+database
+  .query("select * from users")
+  .then(([users]) => {
+    console.log(users);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 module.exports = database;
